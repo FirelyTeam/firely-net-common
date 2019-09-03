e4a790434d789f3c84e9493549a80c434b21877e
Ewout Kramer <e.kramer@furore.com>
2019-09-03 14:40:38 +0200

    feature: added Cast class and some tests

---
+++ src/Hl7.Fhir.Support/Language/OptionTypeSpecifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hl7.Fhir.Language
+{
+    public class OptionTypeSpecifier : TypeSpecifier
+    {
+        public OptionTypeSpecifier(TypeSpecifier option)
+        {
+            Option = option ?? throw new ArgumentNullException(nameof(option));
+        }
+
+        public TypeSpecifier Option { get; private set; }
+
+        public override string ToString() => Option.ToString() + "?";
+    }
+
+}
