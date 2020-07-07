70c5f8a3ebc5b593f7de50f0a328626f415ac0f3
Ewout Kramer <e.kramer@furore.com>
2020-07-07 15:45:37 +0200

    feature: Added Result<T> class

---
+++ src/Hl7.Fhir.Support.Tests/Utility/ResultTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Hl7.Fhir.Support.Utility.Result;
+
+namespace Hl7.Fhir.Support.Utility.Tests
+{
+    [TestClass]
+    public class ResultTests
+    {
+        [TestMethod]
+        public void ResultsTest()
+        {
+            var r = Ok(4);
+            Assert.AreEqual(10, r + 6);
+
+            Result<int> resultInt = 5;
+            Assert.AreEqual(Ok(5), resultInt);
+
+            var ex = new NotFiniteNumberException();
+            var e = Fail<int>(ex);
+            Assert.AreEqual(Fail<int>(ex),e);
+
+            //Console.WriteLine(r3.Try());
+            //try { Console.WriteLine(e.Try()); } catch(NotFiniteNumberException) { Console.WriteLine("No value!");  }
+
+            Assert.AreEqual(8,e.ValueOrDefault(8));
+            Assert.AreEqual(ex.Message.Length,e.ValueOrElse(e => e.Message.Length));
+
+            Assert.AreEqual(Ok(true),resultInt.Chain(i => Ok(i*2).Chain(j => Ok(j > i))));
+            Assert.AreEqual(e.Error, ((Fail<string>)e.Chain(_ => Ok($"Success!"))).Error);
+
+            Assert.AreEqual(Ok(9),
+                                from a in r
+                                from b in resultInt
+                                select a + b);
+
+            Assert.AreEqual(e,
+                                from a in r
+                                from b in e
+                                select a + b);
+
+            Assert.AreEqual("Success: 5", resultInt.Handle(v => $"Success: {v}", e => $"Failed with: {e.Message}"));
+            Assert.AreEqual("Failed with: " + ex.Message, e.Handle(v => $"Success: {v}", e => $"Failed with: {e.Message}"));
+
+            Assert.AreEqual(Ok(5), r.Combine(resultInt));
+            Assert.AreEqual(e, r.Combine(e));
+            Assert.AreEqual(e, e.Combine(r));
+
+            Assert.AreEqual(Ok(5), r & resultInt);
+            Assert.AreEqual(e, r & e);
+            Assert.AreEqual(e, e & r);
+
+            Assert.IsTrue(Ok(5) == Ok(5));
+            Assert.IsTrue(Ok(5) != Ok(6));
+            Assert.IsTrue(Fail(ex) == Fail(ex));
+            Assert.IsTrue(Fail(ex) != Fail(new ArgumentException()));
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Support/Utility/Unit.cs
@@ -0,0 +1,18 @@
+﻿/* 
+ * Copyright (c) 2020, Firely (info@fire.ly) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/fhir-net-api/master/LICENSE
+ */
+
+namespace Hl7.Fhir.Support.Utility
+{
+    public struct Unit
+    {
+        public override bool Equals(object obj) => obj is Unit;
+        public override int GetHashCode() => 0;
+        public override string ToString() => "unit value";
+    }
+
+}
