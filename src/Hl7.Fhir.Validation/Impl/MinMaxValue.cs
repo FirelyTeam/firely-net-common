0647279daf9d520cc8cae3e022573663961d7674
Marco Visser <m.visser@furore.com>
2020-07-30 17:09:53 +0200

    Use for comparison: Any.ICqlOrderable.CompareTo()

--- src/Hl7.Fhir.Validation/Impl/MinMaxValue.cs
+++ src/Hl7.Fhir.Validation/Impl/MinMaxValue.cs
@@ -6,7 +6,9 @@
  * available at https://raw.githubusercontent.com/FirelyTeam/fhir-net-api/master/LICENSE
  */
 
+
 using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.ElementModel.Types;
 using Hl7.Fhir.Serialization;
 using Hl7.Fhir.Utility;
 using Hl7.Fhir.Validation.Schema;
@@ -29,16 +31,28 @@ namespace Hl7.Fhir.Validation.Impl
         private readonly ITypedElement _minMaxValue;
         private readonly MinMax _minMaxType;
         private readonly string _key;
+        private Any _minMaxAnyValue;
+        private readonly int _comparisonOutcome;
+        private readonly string _comparisonLabel;
+        private readonly Issue _comparisonIssue;
 
         public MinMaxValue(ITypedElement minMaxValue, MinMax minMaxType)
         {
             _minMaxValue = minMaxValue ?? throw new ArgumentNullException($"{nameof(minMaxValue)} cannot be null");
             _minMaxType = minMaxType;
 
+            _minMaxAnyValue = Any.Convert(_minMaxValue.Value);
+            _comparisonOutcome = _minMaxType == MinMax.MinValue ? -1 : 1;
+            _comparisonLabel = _comparisonOutcome == -1 ? "smaller than" :
+                                    _comparisonOutcome == 0 ? "equal to" :
+                                        "larger than";
+            _comparisonIssue = _comparisonOutcome == -1 ? Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_TOO_SMALL :
+                                           Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_TOO_LARGE;
+
             _key = $"{_minMaxType.GetLiteral().Uncapitalize()}[x]";
 
             // Min/max are only defined for ordered types
-            if (!IsOrderedType(_minMaxValue.InstanceType))
+            if (!IsOrderedType(_minMaxValue.Value))
             {
                 throw new IncorrectElementDefinitionException($"{_minMaxValue.Name} was given in ElementDefinition, but type '{_minMaxValue.InstanceType}' is not an ordered type");
             }
@@ -52,25 +66,21 @@ namespace Hl7.Fhir.Validation.Impl
 
         public override Task<Assertions> Validate(ITypedElement input, ValidationContext vc)
         {
-            var comparisonOutcome = _minMaxType == MinMax.MinValue ? -1 : 1;
-
-            // TODO : what to do if Value is not IComparable?
-            if (input.Value is IComparable instanceValue)
+            if (!Any.TryConvert(input.Value, out var instanceValue))
             {
-                if (input.InstanceType != _minMaxValue.InstanceType)
-                    return Task.FromResult(Assertions.Empty + ResultAssertion.CreateFailure(new IssueAssertion(Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_NOT_COMPARABLE, input.Location, $"Value '{instanceValue}' cannot be compared with {_minMaxValue.Value})")));
+                return Task.FromResult(Assertions.Empty + ResultAssertion.CreateFailure(new IssueAssertion(Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_NOT_COMPARABLE, input.Location, $"Value '{input.Value}' cannot be compared with {_minMaxValue.Value})")));
+            }
 
-                if (instanceValue.CompareTo(_minMaxValue.Value) == comparisonOutcome)
+            try
+            {
+                if ((instanceValue is ICqlOrderable ce ? ce.CompareTo(_minMaxAnyValue) : -1) == _comparisonOutcome)
                 {
-                    var label = comparisonOutcome == -1 ? "smaller than" :
-                                    comparisonOutcome == 0 ? "equal to" :
-                                        "larger than";
-
-                    var issue = comparisonOutcome == -1 ? Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_TOO_SMALL :
-                                           Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_TOO_LARGE;
-
-                    return Task.FromResult(Assertions.Empty + ResultAssertion.CreateFailure(new IssueAssertion(issue, input.Location, $"Value '{instanceValue}' is {label} {_minMaxValue.Value})")));
+                    return Task.FromResult(Assertions.Empty + ResultAssertion.CreateFailure(new IssueAssertion(_comparisonIssue, input.Location, $"Value '{input.Value}' is {_comparisonLabel} {_minMaxValue.Value})")));
                 }
+            }
+            catch (ArgumentException)
+            {
+                return Task.FromResult(Assertions.Empty + ResultAssertion.CreateFailure(new IssueAssertion(Issue.CONTENT_ELEMENT_PRIMITIVE_VALUE_NOT_COMPARABLE, input.Location, $"Value '{input.Value}' cannot be compared with {_minMaxValue.Value})")));
             }
 
             return Task.FromResult(Assertions.Success);
@@ -84,24 +94,11 @@ namespace Hl7.Fhir.Validation.Impl
         /// <summary>
         /// TODO Validation: this should be altered and moved to a more generic place, and should be more sophisticated
         /// </summary>
-        /// <param name="type"></param>
+        /// <param name="value"></param>
         /// <returns></returns>
-        private bool IsOrderedType(string type)
+        private bool IsOrderedType(object value)
         {
-            switch (type)
-            {
-                case "date":
-                case "dateTime":
-                case "instant":
-                case "time":
-                case "decimal":
-                case "integer":
-                case "positiveInt":
-                case "unsignedInt":
-                case "Quantity": return true;
-                default:
-                    return false;
-            }
+            return Any.TryConvert(value, out _minMaxAnyValue) && _minMaxAnyValue is ICqlOrderable;
         }
     }
 }
