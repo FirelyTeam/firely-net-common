7419957e5574051ea362b7c1dcb8ebf15a2f12b6
Ewout Kramer <e.kramer@furore.com>
2021-01-29 17:36:41 +0100

    Moved ResourceIdentity + ResourceIdentityExtensions to common

--- src/Hl7.Fhir.Support.Poco/Model/Resource.cs
+++ src/Hl7.Fhir.Support.Poco/Model/Resource.cs
@@ -41,30 +41,6 @@ namespace Hl7.Fhir.Model
     public partial class Resource
     {
         /// <summary>
-        /// Codes indicating the type of validation to perform
-        /// </summary>
-        //[FhirEnumeration("ResourceValidationMode")]
-        //public enum ResourceValidationMode
-        //{
-        //    /// <summary>
-        //    /// The server checks the content, and then checks that the content would be acceptable as a create (e.g. that the content would not violate any uniqueness constraints).
-        //    /// </summary>
-        //    [EnumLiteral("create", "http://hl7.org/fhir/resource-validation-mode")]
-        //    Create,
-        //    /// <summary>
-        //    /// The server checks the content, and then checks that it would accept it as an update against the nominated specific resource (e.g. that there are no changes to immutable fields the server does not allow to change, and checking version integrity if appropriate).
-        //    /// </summary>
-        //    [EnumLiteral("update", "http://hl7.org/fhir/resource-validation-mode")]
-        //    Update,
-        //    /// <summary>
-        //    /// The server ignores the content, and checks that the nominated resource is allowed to be deleted (e.g. checking referential integrity rules).
-        //    /// </summary>
-        //    [EnumLiteral("delete", "http://hl7.org/fhir/resource-validation-mode")]
-        //    Delete,
-        //}
-
-
-        /// <summary>
         /// This is the base URL of the FHIR server that this resource is hosted on
         /// </summary>
         public Uri ResourceBase
