362696702b4515b6f55c07252d1c5f152fb9cb0f
Marten Smits <m.smits@furore.com>
2021-02-08 16:21:37 +0100

    feature: add utilities to parse fhir release versions

---
+++ src/Hl7.Fhir.Support.Poco/Utility/FhirVersionUtility.cs
@@ -0,0 +1,112 @@
+﻿using Hl7.Fhir.Specification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hl7.Fhir.Utility
+{
+    public class FhirReleaseParser
+    {
+        /// <summary>
+        /// Returns a FHIR version as an enum from a version number
+        /// </summary>
+        /// <param name="version">Fhir Release version number</param>
+        /// <returns>Official FHIR Release</returns>
+        public static FhirRelease? Parse(string version)
+        {
+            return version switch
+            {
+                "0.01" => FhirRelease.DSTU1,
+                "0.05" => FhirRelease.DSTU1,
+                "0.06" => FhirRelease.DSTU1,
+                "0.11" => FhirRelease.DSTU1,
+                "0.0.80" => FhirRelease.DSTU1,
+                "0.0.81" => FhirRelease.DSTU1,
+                "0.0.82" => FhirRelease.DSTU1,
+
+                "0.4.0" => FhirRelease.DSTU2,
+                "0.5.0" => FhirRelease.DSTU2,
+                "1.0.0" => FhirRelease.DSTU2,
+                "1.0.1" => FhirRelease.DSTU2,
+                "1.0.2" => FhirRelease.DSTU2,
+
+                "1.1.0" => FhirRelease.STU3,
+                "1.4.0" => FhirRelease.STU3,
+                "1.6.0" => FhirRelease.STU3,
+                "1.8.0" => FhirRelease.STU3,
+                "3.0.0" => FhirRelease.STU3,
+                "3.0.1" => FhirRelease.STU3,
+                "3.0.2" => FhirRelease.STU3,
+
+                "3.2.0" => FhirRelease.R4,
+                "3.3.0" => FhirRelease.R4,
+                "3.5.0" => FhirRelease.R4,
+                "3.5a.0" => FhirRelease.R4,
+                "3.6.0" => FhirRelease.R4,
+                "4.0.0" => FhirRelease.R4,
+                "4.0.1" => FhirRelease.R4,
+
+                "4.2.0" => FhirRelease.R5,
+                "4.4.0" => FhirRelease.R5,
+                "4.5.0" => FhirRelease.R5,
+                "5.0.0" => FhirRelease.R5,
+                _ => null
+            };
+        }
+
+        /// <summary>
+        /// Returns the version number of the latest official FHIR releases
+        /// </summary>
+        /// <param name="fhirRelease">Official FHIR release</param>
+        /// <returns>Latest version number</returns>
+        public static string FhirVersionFromRelease(FhirRelease fhirRelease)
+        {
+            return fhirRelease switch
+            {
+                FhirRelease.DSTU1 => "0.0.82",
+                FhirRelease.DSTU2 => "1.0.2",
+                FhirRelease.STU3 => "3.0.2",
+                FhirRelease.R4 => "4.0.1",
+                FhirRelease.R5 => "4.5.0",
+                _ => throw new Exception($"Unknown FHIR version {fhirRelease}")
+            };
+        }
+
+        /// <summary>
+        /// Returns the official FHIR version based on the value of a MIME-Type parameter 'fhirversion'
+        /// </summary>
+        /// <param name="fhirMimeVersion">'fhirversion' MIME-Type parameter</param>
+        /// <returns>Official FHIR Release</returns>
+        public static FhirRelease? FhirReleaseFromMimeVersion(string fhirMimeVersion)
+        {
+            // source: https://www.hl7.org/fhir/http.html#version-parameter
+            return fhirMimeVersion switch
+            {
+                "0.0" => FhirRelease.DSTU1,
+                "1.0" => FhirRelease.DSTU2,
+                "3.0" => FhirRelease.STU3,
+                "4.0" => FhirRelease.R4,
+                "5.0" => FhirRelease.R5,
+                _ => null
+            };
+        }
+
+        /// <summary>
+        ///  Returns the value of the 'fhirversion' MIME-type parameter corresponding to a specific FHIR Version
+        /// </summary>
+        /// <param name="fhirRelease">Official FHIR release</param>
+        /// <returns>Corresponding 'fhirversion' MIME-Type value</returns>
+        public static string MimeVersionFromFhirRelease(FhirRelease fhirRelease)
+        {
+            return fhirRelease switch
+            {
+                FhirRelease.DSTU1 => "0.0",
+                FhirRelease.DSTU2 => "1.0",
+                FhirRelease.STU3 => "3.0",
+                FhirRelease.R4 => "4.0",
+                FhirRelease.R5 => "5.0",
+                _ => null
+            };
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Support.Tests/Utility/FhirVersionTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hl7.Fhir.Utility;
+using Hl7.Fhir.Specification;
+
+namespace Hl7.Fhir.Utility.Tests
+{
+    [TestClass]
+    public class FhirVersionTests
+    {
+        [TestMethod]
+        public void TestFhirReleaseFromVersion()
+        {
+            Assert.IsNull(FhirReleaseParser.Parse("0.0.1"));
+            Assert.AreEqual(FhirRelease.DSTU1, FhirReleaseParser.Parse("0.01"));
+            Assert.AreEqual(FhirRelease.DSTU1, FhirReleaseParser.Parse("0.11"));
+            Assert.AreEqual(FhirRelease.DSTU1, FhirReleaseParser.Parse("0.0.80"));
+
+            Assert.AreEqual(FhirRelease.DSTU2, FhirReleaseParser.Parse("0.4.0"));
+            Assert.AreEqual(FhirRelease.DSTU2, FhirReleaseParser.Parse("1.0.2"));
+
+            Assert.AreEqual(FhirRelease.STU3, FhirReleaseParser.Parse("1.8.0"));
+            Assert.AreEqual(FhirRelease.STU3, FhirReleaseParser.Parse("3.0.0"));
+            Assert.AreEqual(FhirRelease.STU3, FhirReleaseParser.Parse("3.0.2"));
+
+            Assert.AreEqual(FhirRelease.R4, FhirReleaseParser.Parse("3.5a.0"));
+            Assert.AreEqual(FhirRelease.R4, FhirReleaseParser.Parse("3.6.0"));
+            Assert.AreEqual(FhirRelease.R4, FhirReleaseParser.Parse("4.0.0"));
+            Assert.AreEqual(FhirRelease.R4, FhirReleaseParser.Parse("4.0.1"));
+
+            Assert.AreEqual(FhirRelease.R5, FhirReleaseParser.Parse("4.2.0"));
+            Assert.AreEqual(FhirRelease.R5, FhirReleaseParser.Parse("4.5.0"));
+            Assert.AreEqual(FhirRelease.R5, FhirReleaseParser.Parse("5.0.0"));
+        }
+
+        [TestMethod]
+        public void TestsFhirVersionFromRelease()
+        {            
+            Assert.AreEqual("0.0.82", FhirReleaseParser.FhirVersionFromRelease(FhirRelease.DSTU1));
+            Assert.AreEqual("1.0.2", FhirReleaseParser.FhirVersionFromRelease(FhirRelease.DSTU2));
+            Assert.AreEqual("3.0.2", FhirReleaseParser.FhirVersionFromRelease(FhirRelease.STU3));
+            Assert.AreEqual("4.0.1", FhirReleaseParser.FhirVersionFromRelease(FhirRelease.R4));
+            Assert.AreEqual("4.5.0", FhirReleaseParser.FhirVersionFromRelease(FhirRelease.R5));
+        }
+
+        [TestMethod]
+        public void TestsMimeVersionFromRelease()
+        {
+            Assert.AreEqual("0.0", FhirReleaseParser.MimeVersionFromFhirRelease(FhirRelease.DSTU1));
+            Assert.AreEqual("1.0", FhirReleaseParser.MimeVersionFromFhirRelease(FhirRelease.DSTU2));
+            Assert.AreEqual("3.0", FhirReleaseParser.MimeVersionFromFhirRelease(FhirRelease.STU3));
+            Assert.AreEqual("4.0", FhirReleaseParser.MimeVersionFromFhirRelease(FhirRelease.R4));
+            Assert.AreEqual("5.0", FhirReleaseParser.MimeVersionFromFhirRelease(FhirRelease.R5));
+        }
+
+        [TestMethod]
+        public void TestsFhirVersionFromMimeVersion()
+        {
+            Assert.AreEqual(FhirRelease.DSTU1, FhirReleaseParser.FhirReleaseFromMimeVersion("0.0"));
+            Assert.AreEqual(FhirRelease.DSTU2, FhirReleaseParser.FhirReleaseFromMimeVersion("1.0"));
+            Assert.AreEqual(FhirRelease.STU3, FhirReleaseParser.FhirReleaseFromMimeVersion("3.0"));
+            Assert.AreEqual(FhirRelease.R4, FhirReleaseParser.FhirReleaseFromMimeVersion("4.0"));
+            Assert.AreEqual(FhirRelease.R5, FhirReleaseParser.FhirReleaseFromMimeVersion("5.0"));
+
+        }
+
+
+
+    }
+}
