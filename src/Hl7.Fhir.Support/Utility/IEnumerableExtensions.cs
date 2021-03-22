928cfad23094d469c8af17ab3a1da923d263d9d0
Marten Smits <m.smits@furore.com>
2021-03-22 16:55:23 +0100

    Added IEnumerable extension to check for duplicates

---
+++ src/Hl7.Fhir.Support/Utility/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿/* 
+ * Copyright (c) 2021, Firely (info@fire.ly) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/firely-net-sdk/master/LICENSE
+ */
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hl7.Fhir.Utility
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool ContainsDuplicates<T>(this IEnumerable<T> enumerable)
+        {
+            var knownKeys = new HashSet<T>();
+            return enumerable.Any(i => !knownKeys.Add(i));
+        }
+    }
+}
