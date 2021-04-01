ad3fb3f69b5b5d04ede8d4b50bb3e557a1519cef
Marten Smits <m.smits@furore.com>
2021-04-01 11:53:10 +0200

    fix: add null check

--- src/Hl7.Fhir.Support.Poco/Model/ParametersExtensions.cs
+++ src/Hl7.Fhir.Support.Poco/Model/ParametersExtensions.cs
@@ -11,12 +11,13 @@ namespace Hl7.Fhir.Model
     {
         public static bool TryGetDuplicates(this Parameters parameters , out IEnumerable<string> duplicates)
         {
-            duplicates =  parameters.Parameter?.Select(p => p.Name)?.GroupBy(x => x)
+            duplicates =  parameters.Parameter?.Select(p => p.Name)?
+                          .GroupBy(x => x)
                           .Where(g => g.Count() > 1)
                           .Select(y => y.Key)
                           .ToList();
 
-            return duplicates.Any();
+            return duplicates?.Any() == true;
         }
     }
 }
