a4df9717d9e1a7dc1bdbf9793efe972a0528173a
Ewout Kramer <e.kramer@furore.com>
2021-05-20 23:00:59 +0200

    Started separation between pure reflection and FHIR metadata info using ReflectionContext

--- src/Hl7.Fhir.Support.Poco/Serialization/FhirReflection.cs
+++ src/Hl7.Fhir.Support.Poco/Serialization/FhirReflection.cs
@@ -1,5 +1,4 @@
 ﻿using Hl7.Fhir.Introspection;
-using Hl7.Fhir.Specification;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -8,66 +7,82 @@ using System.Reflection;
 
 namespace Hl7.Fhir.Serialization
 {
+    public class FhirReflectionContext
+    {
+        public static FhirReflectionContext FromTypeName(Assembly a, string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FhirReflectionContext(Type pocoType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public static class FhirReflection
     {
-        private static readonly ConcurrentDictionary<string, IReadOnlyDictionary<string, Type>> _fhirTypes = new();
-        private static readonly ConcurrentDictionary<Type, FhirTypeAttribute> _fhirTypeAttributes = new();
-
-        private static readonly Dictionary<string, Type> _empty = new();
+        private static readonly ConcurrentDictionary<string, IReadOnlyDictionary<string, IEnumerable<Type>>> _fhirTypes = new();
+        private static readonly ConcurrentDictionary<Type, IReadOnlyDictionary<string, IEnumerable<PropertyInfo>>> _fhirTypeProperties = new();
+        private static readonly ConcurrentDictionary<Type, IEnumerable<FhirTypeAttribute>> _fhirTypeAttributes = new();
+        private static readonly ConcurrentDictionary<PropertyInfo, IEnumerable<FhirElementAttribute>> _fhirElementAttributes = new();
 
         /// <summary>
         /// List all POCO's representing FHIR types in the given assembly, including assemblies containing
         /// FHIR types that the given assembly refers to.
         /// </summary>
-        public static IReadOnlyDictionary<string, Type> GetFhirTypes(this Assembly modelAssembly)
+        public static IReadOnlyDictionary<string, IEnumerable<Type>> GetFhirTypes(this Assembly modelAssembly)
         {
-            return getTypesFromAssembly(modelAssembly.GetName());
+            return _fhirTypes.GetOrAdd(modelAssembly.FullName,
+                getTypesFromAssembly(modelAssembly.GetName())
+                    .ToLookup(p => p.typeName, p => p.type)
+                    .ToDictionary(lu => lu.Key, lu => lu.AsEnumerable(), StringComparer.OrdinalIgnoreCase));
 
-            static IReadOnlyDictionary<string, Type> getTypesFromAssembly(AssemblyName an)
+            static IEnumerable<(string typeName, Type type)> getTypesFromAssembly(AssemblyName an)
             {
                 Assembly a = Assembly.Load(an);
-                //if(a.GetCustomAttribute<FhirModelAssembly> is null) return _empty;
+                if (a.GetCustomAttribute<FhirModelAssemblyAttribute>() is null) return Enumerable.Empty<(string, Type)>();
 
-                return _fhirTypes.GetOrAdd(a.FullName,
+                return
                     a.ExportedTypes
-                    .Where(et => et.RepresentsFhirType())
-                    .Select(et => new KeyValuePair<string, Type>(et.Name, et))
-                    .Concat(a.GetReferencedAssemblies()
-                    .SelectMany(ra => getTypesFromAssembly(ra)))
-                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+                    .SelectMany(et => et.GetFhirTypeAttributes().Select(ta => (ta.Name, et)))
+                    .Concat(
+                        a.GetReferencedAssemblies()
+                        .SelectMany(ra => getTypesFromAssembly(ra)));
             }
         }
 
+        public static IEnumerable<FhirTypeAttribute> GetFhirTypeAttributes(this Type t) =>
+            _fhirTypeAttributes.GetOrAdd(t, t.GetTypeInfo().GetCustomAttributes<FhirTypeAttribute>());
 
-        public static IReadOnlyDictionary<string, Type> GetFhirTypes(this Assembly modelAssembly, FhirRelease filter) => throw new NotImplementedException();
+        public static IReadOnlyDictionary<string, IEnumerable<PropertyInfo>> GetFhirProperties(this Type t) =>
+            _fhirTypeProperties.GetOrAdd(t,
+                t.GetTypeInfo().GetProperties()
+                .SelectMany(pi => pi.GetFhirElementAttributes()
+                    .Select(ea => new KeyValuePair<string, PropertyInfo>(ea.Name, pi)))
+                .ToLookup(p => p.Key, p => p.Value)
+                .ToDictionary(lu => lu.Key, lu => lu.AsEnumerable()));
 
-        /// <summary>
-        /// List all POCO's that represent the given FHIR type. When assemblies for multiple FHIR
-        /// releases are present, there might be more than one type returned.
-        /// </summary>
-        /// <remarks>Handle Any subclasses too</remarks>
-        public static Type GetFhirTypeByName(this Assembly assemblies, string fhirTypeName,
-            StringComparison comparison = StringComparison.Ordinal) => throw new NotImplementedException();
+        public static IEnumerable<FhirElementAttribute> GetFhirElementAttributes(this PropertyInfo pi) =>
+            _fhirElementAttributes.GetOrAdd(pi, pi.GetCustomAttributes<FhirElementAttribute>());
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <remarks>Will return concrete backbone type names using the A#b notation. And also
-        /// know about Any types</remarks>
-        public static string GetFhirTypeName(this Type t) => throw new NotImplementedException();
+        ///// <summary>
+        ///// List all POCO's that represent the given FHIR type. When assemblies for multiple FHIR
+        ///// releases are present, there might be more than one type returned.
+        ///// </summary>
+        ///// <remarks>Handle Any subclasses too</remarks>
+        //public static IEnumerable<Type> GetFhirTypeByName(this Assembly assemblies, string fhirTypeName) =>
+        //            assemblies.GetFhirTypes().TryGetValue(fhirTypeName, out var types) ?
+        //                types
+        //                : Enumerable.Empty<Type>();
 
-        public static IDictionary<string, PropertyInfo> GetFhirProperties(this Type t) => throw new NotImplementedException();
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <remarks>Will return concrete backbone type names using the A#b notation. And also
+        ///// know about Any types</remarks>
+        //public static string GetFhirTypeName(this Type t) => throw new NotImplementedException();
 
-
-        private static FhirTypeAttribute getFhirTypeAttribute(Type t) =>
-                    _fhirTypeAttributes.GetOrAdd(t, t.GetTypeInfo().GetCustomAttribute<FhirTypeAttribute>());
-
-        /// <summary>
-        /// Determines whether the type represents a FHIR datatype or resource.
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public static bool RepresentsFhirType(this Type t) => getFhirTypeAttribute(t) is not null;
 
         /// <summary>
         /// If the property is a choice element, returns the types allowed for the choicse
