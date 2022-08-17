51f1ab960d94900a9a8a8fb8ff309167b5d8567b
Marco Visser <marco@fire.ly>
2022-08-17 10:16:43 +0200

    Revert "We broke the public interface by removing the MAX_FP_EXPRESSION_CACHE_SIZE from IValueProviderFPExtensions."
    This reverts commit a3828cd63fb80169b70e6b8cc7a49a5adb1d118b.

--- src/Hl7.FhirPath/FhirPath/TypedElementFPExtensions.cs
+++ src/Hl7.FhirPath/FhirPath/TypedElementFPExtensions.cs
@@ -9,7 +9,6 @@
 #nullable enable
 
 using Hl7.Fhir.ElementModel;
-using System;
 using System.Collections.Generic;
 
 namespace Hl7.FhirPath
@@ -17,39 +16,27 @@ namespace Hl7.FhirPath
 
     public static class IValueProviderFPExtensions
     {
-        public static int MAX_FP_EXPRESSION_CACHE_SIZE = FhirPathCompilerCache.DEFAULT_FP_EXPRESSION_CACHE_SIZE;
-
-        private static Lazy<FhirPathCompilerCache> CACHE = new(() => new(compiler: null, cacheSize: MAX_FP_EXPRESSION_CACHE_SIZE));
+        private static readonly FhirPathCompilerCache CACHE = new();
 
         /// <inheritdoc cref="FhirPathCompilerCache.Select(ITypedElement, string, EvaluationContext?)"/>
         public static IEnumerable<ITypedElement> Select(this ITypedElement input, string expression, EvaluationContext? ctx = null)
-            => CACHE.Value.Select(input, expression, ctx);
+            => CACHE.Select(input, expression, ctx);
 
         /// <inheritdoc cref="FhirPathCompilerCache.Scalar(ITypedElement, string, EvaluationContext?)"/>
         public static object? Scalar(this ITypedElement input, string expression, EvaluationContext? ctx = null)
-            => CACHE.Value.Scalar(input, expression, ctx);
+            => CACHE.Scalar(input, expression, ctx);
 
         /// <inheritdoc cref="FhirPathCompilerCache.Predicate(ITypedElement, string, EvaluationContext?)"/>
         public static bool Predicate(this ITypedElement input, string expression, EvaluationContext? ctx = null)
-            => CACHE.Value.Predicate(input, expression, ctx);
+            => CACHE.Predicate(input, expression, ctx);
 
         /// <inheritdoc cref="FhirPathCompilerCache.IsTrue(ITypedElement, string, EvaluationContext?)"/>
         public static bool IsTrue(this ITypedElement input, string expression, EvaluationContext? ctx = null)
-            => CACHE.Value.IsTrue(input, expression, ctx);
+            => CACHE.IsTrue(input, expression, ctx);
 
         /// <inheritdoc cref="FhirPathCompilerCache.IsBoolean(ITypedElement, string, bool, EvaluationContext?)"/>
         public static bool IsBoolean(this ITypedElement input, string expression, bool value, EvaluationContext? ctx = null)
-            => CACHE.Value.IsBoolean(input, expression, value, ctx);
-
-        /// <summary>
-        /// Reinitialize the cache. This method is only meant for the unit tests, but can be made public later. We need some refactoring here, I (MV) think.
-        /// </summary>
-        /// <param name="compiler">A userdefined compiler</param>
-        /// <param name="cacheSize">the new size for the cache</param>
-        internal static void ReInitializeCache(FhirPathCompiler? compiler = null, int? cacheSize = null)
-        {
-            CACHE = new(() => new(compiler, cacheSize ?? MAX_FP_EXPRESSION_CACHE_SIZE));
-        }
+            => CACHE.IsBoolean(input, expression, value, ctx);
     }
 }
 
